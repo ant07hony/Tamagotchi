327ab952150fd3a1ff5f301bb15ac52c25b0f719
ant07hony <ant07hony@yahoo.com>
2023-03-08 22:01:03 -0500

    startFace complete

--- Tamagotchi/js/app.js
+++ Tamagotchi/js/app.js
@@ -1,8 +1,8 @@
-  /*----- constants -----*/
+/*----- constants -----*/
 const faces = {
-    happy: '\( ͡> ω ͡<)/',
-    sad: '  \( ͡> ﹏ ͡<)/',
-    angry: '\( ͡> ෴ ͡<)/',
+    happy: '\( ͡> ω ͡<)/', //0-49
+    sad: '  \( ͡> ﹏ ͡<)/', //50-79
+    angry: '\( ͡> ෴ ͡<)/', //80-100
 }
 
 /*----- state variables -----*/
@@ -12,74 +12,112 @@ const faces = {
 
 //handleClick()
 
-  /*----- cached elements  -----*/
+/*----- cached elements  -----*/
 
-  const playBtn = document.querySelector('.playBtn')
-  const yummyBtn = document.querySelector('.yummyBtn')
-  const pottyBtn = document.querySelector('.pottyBtn')
-  
-  const name = document.querySelector('#name').innerHTML = 'Name: '
+const btns = document.querySelectorAll('button')
+const playBtn = document.querySelector('.playBtn')
+const yummyBtn = document.querySelector('.yummyBtn')
+const pottyBtn = document.querySelector('.pottyBtn')
 
-  const playLabel = document.querySelector('.playH3').innerHTML = 'Play!'
+const name = document.querySelector('#name').innerHTML = 'Name: '
 
-  const yummyLabel = document.querySelector('.yummyH3').innerHTML = 'Yum!'
+const playLabel = document.querySelector('.playH3').innerHTML = 'Play!'
 
-  const pottyLabel = document.querySelector('.pottyH3').innerHTML = 'Potty!'
+const yummyLabel = document.querySelector('.yummyH3').innerHTML = 'Yum!'
 
-  /*----- event listeners -----*/
+const pottyLabel = document.querySelector('.pottyH3').innerHTML = 'Potty!'
+
+/*----- event listeners -----*/
 // need event listeners for each button to add time to each respective bar
-
+btns.forEach(function(btn) {
+    btn.addEventListener('click', handleClick)
+})
 // playBtn.addEventListener('click', handleClick)
 // yummyBtn.addEventListener('click', handleClick)
 // pottyBtn.addEventListener('click', handleClick)
 
-  
+
 
 /*----- functions -----*/
 function init() {
     console.log('init works')
+
     
-    //move()
+    startFace()
+    potty()
+    play()
+    yum()
     render()
 }
 
 function render() {
-console.log('render works')
+    console.log('render works')
 }
-  
+
 function handleClick(evt) {
     const btnClick = evt.target
     //need logic to delay move() when btnClick
-    if( init()) {
-
-
-    }
-}
-
-//dynamic bar
-  function move() {
-    let pottyBar = document.getElementById("pottyBar"); 
-    let pottyBarWidth = 0;  
-    // let barStart = setTimeout(frame, 100)
-    // let barStart = setTimeout(interval, 100)
-    // function interval() {
-    // let pottyBarWidth = 0;
-    // for ( let pottyBarWidth = 0; pottyBarWidth >= 100; pottyBarWidth++){
-        let barStart = setInterval(frame, 200);
-        function frame() {
-            if (pottyBarWidth >= 100 ) {
-                // clearTimeout(barStart)
-                clearInterval(barStart)
-            } else {
-                pottyBarWidth++; 
-                pottyBar.style.width = pottyBarWidth + '%'; 
-                pottyBar.innerHTML = pottyBarWidth * 1  + '%';
-        
-        } 
-        }
-    }
-    //clearTimeout(barStart)
     
 
+    }
 
-  init()
+function startFace() {
+    const tamaFace = document.querySelector('.image')
+    tamaFace.innerText = faces['happy']
+}
+
+
+//bar function for potty
+function potty() {
+    let pottyBar = document.getElementById("pottyBar");
+    let pottyBarWidth = 0;
+    let barStart = setInterval(frame, 200);
+    function frame() {
+        if (pottyBarWidth >= 100) {
+            clearInterval(barStart)
+        } else {
+            pottyBarWidth++;
+            pottyBar.style.width = pottyBarWidth + '%';
+            pottyBar.innerHTML = pottyBarWidth * 1 + '%';
+
+        }
+    } 
+}
+
+//bar function for play
+function play() {
+        let playBar = document.getElementById("playBar");
+        let playBarWidth = 0;
+        let barStart = setInterval(frame, 200);
+        function frame() {
+            if (playBarWidth >= 100) {
+                clearInterval(barStart)
+            } else {
+                playBarWidth++;
+                playBar.style.width = playBarWidth + '%';
+                playBar.innerHTML = playBarWidth * 1 + '%';
+    
+            }
+        }
+    }
+
+
+//bar function for yum
+function yum() {
+        let yumBar = document.getElementById("eatBar");
+        let yumBarWidth = 0;
+        let barStart = setInterval(frame, 200);
+        function frame() {
+            if (yumBarWidth >= 100) {
+                clearInterval(barStart)
+            } else {
+                yumBarWidth++;
+                yumBar.style.width = yumBarWidth + '%';
+                yumBar.innerHTML = yumBarWidth * 1 + '%';
+    
+            }
+        }
+    }
+
+
+init()
