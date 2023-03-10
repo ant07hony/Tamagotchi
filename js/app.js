4297d663a5825733d07bf1ad4e70ae0ac9c035aa
ant07hony <ant07hony@yahoo.com>
2023-03-10 15:08:24 -0500

    MVP version w/o stretch goals

--- js/app.js
+++ js/app.js
@@ -1,172 +1,102 @@
 /*----- constants -----*/
+
 const faces = {
     happy: '\( ͡> ω ͡<)/', //0-49
     sad: '  \( ͡> ﹏ ͡<)/', //50-79
     angry: '\( ͡> ෴ ͡<)/', //80-100
 }
 
-
-
-
 /*----- state variables -----*/
 
-// play, eat and clean btn variables 
-//initialized function is different than Event Listener function
 
-//btnClick()
 
 /*----- cached elements  -----*/
 
-//variable for all button elements
+//cache for all button elements
 const btns = document.querySelectorAll('button')
 
-//variable for all progress bars
+//cache for all progress bars
 const bar = document.querySelectorAll('#bar')
 
-//variables for each individual buttons
+//cache for each individual button
 const playBtn = document.querySelector('#playBtn')
 const eatBtn = document.querySelector('#eatBtn')
 const cleanBtn = document.querySelector('#cleanBtn')
 
-
-//variables for DOM
+//cache for Play! , Eat! , Clean! on screen
 const playText = document.querySelector('.playH3').innerHTML = 'Play!'
 const eatText = document.querySelector('.eatH3').innerHTML = 'Eat!'
 const cleanText = document.querySelector('.cleanH3').innerHTML = 'Clean!'
 
 /*----- event listeners -----*/
-//event listener for all buttons to decrement time of each respective bar
-
+//located with each bar function
 
 
 /*----- functions -----*/
+
 function init() {
-    console.log('init works')
 
     const tamaFace = document.querySelector('.image')
     tamaFace.innerText = faces['happy']
 
-
-
     render()
 }
 
-// ( MVP )
+// ( MVP ) render folder for everything functioning
 function render() {
 
-    //folder for all renders
-
-    //renderFace()
-    //renderName()   //-----works but is annoying in development while making    changes 
+    renderFace()
+    renderName()
     renderAge()
-    //renderPlayBtn()
-    //  renderEatbtn()
-    //  renderCleanBtn()
     renderCleanBar()
-    //renderPlayBar()  //-------this works...yaye
-    // renderEatBar()
+    renderPlayBar()
+    renderEatBar()
 
 
 }
 
-// ( MVP )
+// ( STRETCH )
 function renderFace() {
 
-    //healthy happy face
+    //have faces alternate based on bars progress
 
-    function startFace() {
-        const tamaFace = document.querySelector('.image')
-        tamaFace.innerText = faces['happy']
+    // const tamaFace = document.querySelector('.image')
+    // tamaFace.innerText = faces['happy']
 
-        // const tamaFace = document.querySelector('.image')
-        // tamaFace.innerText = faces['angry']
-    }
-    render()
+    // const tamaFace = document.querySelector('.image')
+    // tamaFace.innerText = faces['angry']
+
+    // const tamaFace = document.querySelector('.image')
+    // tamaFace.innerText = faces['sad']
+
 
 }
 
-//( MVP ) 
+// ( MVP )---ability to name Tamagotchi....or not
 function renderName() {
+
     //prompt input box to name tamagotchi
-    const name = prompt(" Welcome to Tamagotchi Survivor. Please name your 'Tama' ")
+
+    const name = prompt(" Welcome to Tamagotchi Sitter. Today you'll be taking care of ")
     if (name != null) {
         document.getElementById('name').innerHTML = "Name: " + name
     } else {
-        alert(" Have a nice day! ")
-
+        document.getElementById('name').innerHTML = "Name: "
     }
-
-
 }
 
-// ( NOT MVP ) 
+// ( STRETCH )---timer based function to display some age
 function renderAge() {
     //timer to record how long tamagotchi is alive
 
 }
 
-// ( MVP )
-function renderPlayBtn() {
-    //when clicked, will decrease the playBar 5%
-    //playBarBtn is a string
-    //pBBtn is a string
-    // console.log(typeof pBBtn) typeof playBarBtn === 'number' && 
-    let playBarBtn = document.querySelector('#playBar').getAttribute('style')
-    const playBtn = document.querySelector('#playBtn')
+//global scopes incrementing and decrementing
+let playBarWidth = 0
+let eatBarWidth = 0
+let cleanBarWidth = 0
 
-    // let pBBtn = document.querySelector('#playBar').innerHTML
-    // if (pBBtn < 100) {
-    //      pBBtn = pBBtn - 5 + '%'
-    //      pBBtn--
-    //      pBBtn.style.width = playBarWidth + '%';
-    //      playBar.innerHTML = playBarWidth % 1 + '%';
-    // }
-
-}
-
-
-
-
-
-
-
-// ( MVP )
-function renderEatbtn() {
-    //button should cause bar to minus 5%, then continue counting to 100%
-
-    //the attempt
-    function btnClick(evt) {
-        const eatBar = document.getElementById('eatBar')
-        let eatBarWidth = eatBar.style.width
-        //console.log(eatBarWidth)
-        let barStart = setInterval(frame, 400);
-        function frame() {
-            if (eatBarWidth < 100) {
-                eatBarWidth--;
-                eatBar.style.width = eatBarWidth + '%';
-                eatBar.innerHTML = eatBarWidth * 1 + '%';
-                clearInterval(barStart)
-            } else {
-                return
-            }
-        }
-    }
-
-
-}
-
-// ( MVP )
-function renderCleanBtn() {
-    //button should cause bar to minus 5%, then continue counting to 100%
-
-} //not complete
-
-// ( MVP )
-
-
-let barWidth = 0;
-let barStart
-
+// ( MVP )---function to increment the play bar using the play button
 function renderPlayBar() {
 
     //bar starts from 0% and increment to 100% when browser loads
@@ -174,85 +104,74 @@ function renderPlayBar() {
     // playBtn.addEventListener('click', renderPlayBtn)
 
     let playBar = document.getElementById("playBar")
-    let barStart = setInterval(frame, 200);
+    let barStart = setInterval(frame, 100);
     function frame() {
-        if (barWidth >= 100) {
-            barWidth = playBar
+        if (playBarWidth >= 100) {
             clearInterval(barStart)
-            playBtn.removeEventListener('click', function () {
-                barWidth -= 5;
-                playBar.style.width = barWidth + '%'
-                playBar.innerHTML = barWidth * 1 + '%'
-                confirm("Your 'Tama' passed away :(")
-            })
+            confirm("[name] passed away :(")
         } else {
-            barWidth++;
-            playBar.style.width = barWidth + '%';
-            playBar.innerHTML = barWidth * 1 + '%';
-
+            playBarWidth++;
+            playBar.style.width = playBarWidth + '%';
+            playBar.innerHTML = playBarWidth * 1 + '%';
         }
     }
-
-
 }
 
+// (MVP )---function to decrement the play bar using the play button
 playBtn.addEventListener('click', function () {
-    barWidth -= 5;
-    playBar.style.width = barWidth + '%'
-    playBar.innerHTML = barWidth * 1 + '%'
+    playBarWidth -= 5;
+    playBar.style.width = playBarWidth + '%'
+    playBar.innerHTML = playBarWidth * 1 + '%'
 })
 
-// ( MVP )
+// ( MVP )---function to increment the eat bar
 function renderEatBar() {
 
     //bar starts from 0% and increment to 100% when browser loads
     //formula from W3 schools webpage: progress bar center
 
     let eatBar = document.getElementById("eatBar")
-    let barStart = setInterval(frame, 200);
+    // let eatBarWidth = barWidth
+    let barStart = setInterval(frame, 100);
     function frame() {
-        if (playBarWidth >= 100) {
-            playBarWidth = playBar
+        if (eatBarWidth >= 100) {
             clearInterval(barStart)
-            confirm("Your 'Tama' passed away :(")
+            confirm("[name: ...] passed away :(")
         } else {
-            playBarWidth++;
-            playBar.style.width = playBarWidth + '%';
-            playBar.innerHTML = playBarWidth * 1 + '%';
-            // clearInterval(barStart)
+            eatBarWidth++;
+            eatBar.style.width = eatBarWidth + '%';
+            eatBar.innerHTML = eatBarWidth * 1 + '%';
+
         }
-    }
-    console.log(playBar.innerText)
-    if (playBarWidth == playBar.innerText) {
     }
 
 }
 
+// ( MVP )---function to decrement the eat bar using the eat button
 eatBtn.addEventListener('click', function () {
-    barWidth -= 5;
-    playBar.style.width = barWidth + '%'
-    playBar.innerHTML = barWidth * 1 + '%'
+    let eatBar = document.getElementById("eatBar")
+    // let eatBarWidth = barWidth
+    eatBarWidth -= 5;
+    eatBar.style.width = eatBarWidth + '%'
+    eatBar.innerHTML = eatBarWidth * 1 + '%'
 })
 
-// ( MVP )
+// ( MVP )---function to increment the clean bar
 function renderCleanBar() {
 
     //bar starts from 0% and increment to 100% when browser loads
     //formula from W3 schools webpage: progress bar center
 
     let cleanBar = document.getElementById("cleanBar")
-    let barStart = setInterval(frame, 200);
+    let barStart = setInterval(frame, 100);
     function frame() {
-        if (barWidth >= 100) {
-            barWidth = cleanBar
+        if (cleanBarWidth >= 100) {
             clearInterval(barStart)
-           
-                
-            confirm("Your 'Tama' passed away :(")
+            confirm("[name: ...] passed away :(")
         } else {
-            barWidth++;
-            cleanBar.style.width = barWidth + '%';
-            cleanBar.innerHTML = barWidth * 1 + '%';
+            cleanBarWidth++;
+            cleanBar.style.width = cleanBarWidth + '%';
+            cleanBar.innerHTML = cleanBarWidth * 1 + '%';
 
         }
     }
@@ -260,10 +179,11 @@ function renderCleanBar() {
 
 }
 
+// ( MVP )---function to decrement the bar using the clean button
 cleanBtn.addEventListener('click', function () {
-    barWidth -= 5;
-    cleanBar.style.width = barWidth + '%'
-    cleanBar.innerHTML = barWidth * 1 + '%'
+    cleanBarWidth -= 5;
+    cleanBar.style.width = cleanBarWidth + '%'
+    cleanBar.innerHTML = cleanBarWidth * 1 + '%'
 })
 
 
