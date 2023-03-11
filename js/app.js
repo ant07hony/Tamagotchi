6a6b9adf659ec0d386e3633d236e9e4db7665d79
ant07hony <ant07hony@yahoo.com>
2023-03-11 10:39:00 -0500

    Game-over logic complete

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
 /*----- constants -----*/
 
 const faces = {
-    happy: '\( ͡> ω ͡<)/', //0-49
-    sad: '  \( ͡> ﹏ ͡<)/', //50-79
-    angry: '\( ͡> ෴ ͡<)/', //80-100
+    happy: '\( ͡> ω ͡<)/', //0-49%
+    sad: '  \( ͡> ﹏ ͡<)/', //50-79%
+    angry: '\( ͡> ෴ ͡<)/', //80-100%
 }
 
+const name = prompt(" Welcome to 'Gotchi Sitter! Care for your little tama and keep them happy or else they'll run away. Please name your new bundle of joy! ")
+
 /*----- state variables -----*/
+//global scopes incrementing and decrementing
+let playBarWidth = 0
+let eatBarWidth = 0
+let cleanBarWidth = 0
 
 
 
 /*----- cached elements  -----*/
 
-//cache for all button elements
-const btns = document.querySelectorAll('button')
-
-//cache for all progress bars
-const bar = document.querySelectorAll('#bar')
+let btns = document.querySelectorAll('button')
 
 //cache for each individual button
-const playBtn = document.querySelector('#playBtn')
-const eatBtn = document.querySelector('#eatBtn')
-const cleanBtn = document.querySelector('#cleanBtn')
+let playBtn = document.querySelector('#playBtn')
+let eatBtn = document.querySelector('#eatBtn')
+let cleanBtn = document.querySelector('#cleanBtn')
 
 //cache for Play! , Eat! , Clean! on screen
-const playText = document.querySelector('.playH3').innerHTML = 'Play!'
-const eatText = document.querySelector('.eatH3').innerHTML = 'Eat!'
-const cleanText = document.querySelector('.cleanH3').innerHTML = 'Clean!'
+let playText = document.querySelector('.playH3').innerHTML = 'Play!'
+let eatText = document.querySelector('.eatH3').innerHTML = 'Eat!'
+let cleanText = document.querySelector('.cleanH3').innerHTML = 'Clean!'
 
 /*----- event listeners -----*/
-//located with each bar function
+//located line: 158 - 181
 
 
 /*----- functions -----*/
@@ -38,6 +40,8 @@ function init() {
 
     const tamaFace = document.querySelector('.image')
     tamaFace.innerText = faces['happy']
+
+    
 
     render()
 }
@@ -48,10 +52,8 @@ function render() {
     renderFace()
     renderName()
     renderAge()
-    renderCleanBar()
-    renderPlayBar()
-    renderEatBar()
-
+    renderGameOverMessage()
+    renderProgressBar()
 
 }
 
@@ -77,13 +79,14 @@ function renderName() {
 
     //prompt input box to name tamagotchi
 
-    const name = prompt(" Welcome to Tamagotchi Sitter. Today you'll be taking care of ")
     if (name != null) {
         document.getElementById('name').innerHTML = "Name: " + name
     } else {
         document.getElementById('name').innerHTML = "Name: "
     }
+    
 }
+
 
 // ( STRETCH )---timer based function to display some age
 function renderAge() {
@@ -91,110 +94,92 @@ function renderAge() {
 
 }
 
-//global scopes incrementing and decrementing
-let playBarWidth = 0
-let eatBarWidth = 0
-let cleanBarWidth = 0
 
-// ( MVP )---function to increment the play bar using the play button
-function renderPlayBar() {
+
+// ---game logic for game over message
+function renderGameOverMessage() {
+    if ((playBarWidth >= 100 || eatBarWidth >= 100 || cleanBarWidth >= 100) == true ) {
+
+        return confirm(" Oh no! " + name + " ran away ")
+    }
+}
+
+// ( MVP )---game logic for progress bar functionality
+function renderProgressBar() {
 
     //bar starts from 0% and increment to 100% when browser loads
     //formula from W3 schools webpage: progress bar center
-    // playBtn.addEventListener('click', renderPlayBtn)
+    
 
     let playBar = document.getElementById("playBar")
-    let barStart = setInterval(frame, 100);
+    let eatBar = document.getElementById("eatBar")
+    let cleanBar = document.getElementById("cleanBar")
+
+    let barStart = setInterval(frame, 100)
+
     function frame() {
         if (playBarWidth >= 100) {
             clearInterval(barStart)
-            if (confirm("[name] passed away :(") == true) {
-                init()
-            } else {
-                init()
-            }
+            renderGameOverMessage()
+            playBtn.removeEventListener('click', playBarDecrement)
+            eatBtn.removeEventListener('click', eatBarDecrement)
+            cleanBtn.removeEventListener('click', cleanBarDecrement)
         } else {
             playBarWidth++;
             playBar.style.width = playBarWidth + '%';
             playBar.innerHTML = playBarWidth * 1 + '%';
         }
-    }
-}
 
-// (MVP )---function to decrement the play bar using the play button
-playBtn.addEventListener('click', function () {
-    playBarWidth -= 5;
-    playBar.style.width = playBarWidth + '%'
-    playBar.innerHTML = playBarWidth * 1 + '%'
-})
-
-// ( MVP )---function to increment the eat bar
-function renderEatBar() {
-
-    //bar starts from 0% and increment to 100% when browser loads
-    //formula from W3 schools webpage: progress bar center
-
-    let eatBar = document.getElementById("eatBar")
-    // let eatBarWidth = barWidth
-    let barStart = setInterval(frame, 100);
-    function frame() {
-        if (eatBarWidth >= 100) {
-            if (confirm("[name] passed away :(") == true) {
-                init()
-            } else {
-                init()
-            }
-        } else {
-            eatBarWidth++;
-            eatBar.style.width = eatBarWidth + '%';
-            eatBar.innerHTML = eatBarWidth * 1 + '%';
-
-        }
-    }
-
-}
-
-// ( MVP )---function to decrement the eat bar using the eat button
-eatBtn.addEventListener('click', function () {
-    let eatBar = document.getElementById("eatBar")
-    // let eatBarWidth = barWidth
-    eatBarWidth -= 5;
-    eatBar.style.width = eatBarWidth + '%'
-    eatBar.innerHTML = eatBarWidth * 1 + '%'
-})
-
-// ( MVP )---function to increment the clean bar
-function renderCleanBar() {
-
-    //bar starts from 0% and increment to 100% when browser loads
-    //formula from W3 schools webpage: progress bar center
-
-    let cleanBar = document.getElementById("cleanBar")
-    let barStart = setInterval(frame, 100);
-    function frame() {
         if (cleanBarWidth >= 100) {
-            if (confirm("[name] passed away :(") == true) {
-                init()
-            } else {
-                init()
-            }
+            clearInterval(barStart)
+            renderGameOverMessage()
+            playBtn.removeEventListener('click', playBarDecrement)
+            eatBtn.removeEventListener('click', eatBarDecrement)
+            cleanBtn.removeEventListener('click', cleanBarDecrement)
         } else {
             cleanBarWidth++;
             cleanBar.style.width = cleanBarWidth + '%';
             cleanBar.innerHTML = cleanBarWidth * 1 + '%';
+        }
 
+        if (eatBarWidth >= 100) {
+            clearInterval(barStart)
+            renderGameOverMessage()
+            playBtn.removeEventListener('click', playBarDecrement)
+            eatBtn.removeEventListener('click', eatBarDecrement)
+            cleanBtn.removeEventListener('click', cleanBarDecrement)
+        } else {
+            eatBarWidth++;
+            eatBar.style.width = eatBarWidth + '%';
+            eatBar.innerHTML = eatBarWidth * 1 + '%';
         }
     }
+}
 
+// (MVP )---Event listener for play button to decrement the play bar
+playBtn.addEventListener('click', playBarDecrement)
+function playBarDecrement() {
+    playBarWidth -= 5;
+    playBar.style.width = playBarWidth + '%'
+    playBar.innerHTML = playBarWidth * 1 + '%'
 
 }
 
-// ( MVP )---function to decrement the bar using the clean button
-cleanBtn.addEventListener('click', function () {
+// ( MVP )---Event listener for eat button to decrement the eat bar
+eatBtn.addEventListener('click', eatBarDecrement)
+function eatBarDecrement() {
+    eatBarWidth -= 5;
+    eatBar.style.width = eatBarWidth + '%'
+    eatBar.innerHTML = eatBarWidth * 1 + '%'
+}
+
+// ( MVP )---Event listener for clean button to decrement the clean bar
+cleanBtn.addEventListener('click', cleanBarDecrement)
+function cleanBarDecrement() {
     cleanBarWidth -= 5;
     cleanBar.style.width = cleanBarWidth + '%'
     cleanBar.innerHTML = cleanBarWidth * 1 + '%'
-})
+}
 
 
 init()
